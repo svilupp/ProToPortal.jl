379e0c08627236e193af0eeb497ddad1e5e1df39
J S <49557684+svilupp@users.noreply.github.com>
2024-07-12 11:13:15 +0200

    disable caching

--- main.jl
+++ main.jl
@@ -3,4 +3,4 @@ Pkg.activate(".")
 ## Required to support semantic caching
 ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
 using ProToPortal
-ProToPortal.launch(8000, "0.0.0.0"; async = false, cached = true, cache_verbose = true)
+ProToPortal.launch(8000, "0.0.0.0"; async = false, cached = false, cache_verbose = false)
--- src/server.jl
+++ src/server.jl
@@ -2,7 +2,7 @@
     launch(
         port::Int = get(ENV, "PORT", 8000), host::String = get(
             ENV, "GENIE_HOST", "127.0.0.1");
-        async::Bool = true, cached::Bool = true, cache_verbose::Bool = false)
+        async::Bool = true, cached::Bool = false, cache_verbose::Bool = false)
 
 Launches ProToPortal in the browser.
 
@@ -13,20 +13,21 @@ This is a convenience wrapper around `Genie.up`, to customize the server configu
 - `port::Union{Int, String} = get(ENV, "PORT", "8000")`: The port to launch the server on.
 - `host::String = get(ENV, "GENIE_HOST", "127.0.0.1")`: The host to launch the server on.
 - `async::Bool = true`: Whether to launch the server asynchronously, ie, in the background.
-- `cached::Bool = true`: Whether to use semantic caching of the requests.
-- `cache_verbose::Bool = true`: Whether to print verbose information about the caching process.
+- `cached::Bool = false`: Whether to use semantic caching of the requests.
+- `cache_verbose::Bool = false`: Whether to print verbose information about the caching process.
 
 If you want to remove the cache layer later, you can use `import HTTP; HTTP.poplayer!()`.
 """
 function launch(
         port::Union{Int, String} = get(ENV, "PORT", "8000"),
         host::String = get(ENV, "GENIE_HOST", "127.0.0.1");
-        async::Bool = true, cached::Bool = true, cache_verbose::Bool = true)
+        async::Bool = true, cached::Bool = false, cache_verbose::Bool = false)
     ## Loads app.jl in the root directory
     Genie.loadapp(pkgdir(ProToPortal))
 
     ## Enables caching
     ENV["CACHES_VERBOSE"] = cache_verbose ? "true" : "false"
+    ## disable until we can do length-adjusted semantic distance
     if cached
         @info "Caching enabled globally (for all requests, see `CacheLayer` module for details). Remove with `HTTP.poplayer!()`"
         HTTP.pushlayer!(CacheLayer.cache_layer)
